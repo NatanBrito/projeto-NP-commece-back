61bb6d4a759a6224deba2d3d76fb6b00fdea48c5
Natan__Brito <natanismaelbrito@gmail.com>
2022-05-11 16:25:25 -0300

    verify user done

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 import db from "../db.js";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 export async function postRegister(req, res) {
   const body = req.body;
 
@@ -16,4 +18,16 @@ export async function postRegister(req, res) {
   } catch (e) {
     res.status(500).send(e);
   }
+}
+export async function postSignIn(req,res){
+const {email,password}=req.body;
+ try{
+   const user= await db.collection("users").findOne({email});
+   if(user && bcrypt.compareSync(password,user.password)){
+   
+   res.send(user)
+ }
+ }catch(e){
+   res.status(404).send(e)
+ }
 }
