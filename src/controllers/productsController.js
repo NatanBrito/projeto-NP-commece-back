ee6771d4c389e9a23f4eb063055f707eb687734b
Natan__Brito <natanismaelbrito@gmail.com>
2022-05-16 13:06:06 -0300

    send grid

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,6 @@
 import db from "../db.js";
 import dotenv from "dotenv";
+import sgMail from "@sendgrid/mail";
 dotenv.config();
 
 export async function getProducts(req, res) {
@@ -40,4 +41,31 @@ export async function deleteMyProduct(req,res){
   }catch(e){
     res.status(500).send("error delete")
   }
+}
+export async function PostsendDone(req,res){
+  const user= res.locals.user;
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: "cinelli.dev@gmail.com",
+  from: 'natanismaelbrito@gmail.com', // Use the email address or domain you verified above
+  subject: 'Sending with Twilio SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>conferindo a mensagem</strong>',
+};
+//ES6
+sgMail
+  .send(msg)
+  .then(async () => {
+    try {
+      await sgMail.send(msg);
+      console.log("deu certo")
+    } catch (error) {
+      console.error(error);
+  
+      if (error.response) {
+        console.error(error.response.body)
+      }
+    }
+  });
+//ES8
 }
--- src/routers/productsRouter.js
+++ src/routers/productsRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProducts, postProduct, getMyProducts, deleteMyProduct } from "../controllers/productsController.js";
+import { getProducts, postProduct, getMyProducts, deleteMyProduct, PostsendDone } from "../controllers/productsController.js";
 import { validateMyProduct } from "../middlewares/schemaJoiValidations.js";
 import { validateToken } from "../middlewares/validateTokenMiddleware.js";
 
@@ -9,5 +9,6 @@ productsRouter.get("/products", getProducts);
 productsRouter.post("/products", validateMyProduct, validateToken, postProduct);
 productsRouter.get("/myproducts", validateToken, getMyProducts);
 productsRouter.delete("/deletemyproducts/:id", validateToken, deleteMyProduct);
+productsRouter.post("/senddone", validateToken, PostsendDone);
 
 export default productsRouter;
